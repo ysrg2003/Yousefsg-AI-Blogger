f4df9bcd48a3a1a52cc675031cdee8e93375ca26
ysrg2003 <yousefghapn2003@gmail.com>
2026-01-14 13:27:05 +0200

    Update article_generator_advanced.py

--- article_generator_advanced.py
+++ article_generator_advanced.py
@@ -2,8 +2,9 @@ import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 # ضع بياناتك هنا مؤقتاً
-client_id = "ضع_هنا_الـ_client_id"
-client_secret = "ضع_هنا_الـ_client_secret"
+client_id = "291465728218-li940ggvqjc7jid3kvrdi9okjcireiqm.apps.googleusercontent.com"
+client_secret = "{"installed":{"client_id":"291465728218-li940ggvqjc7jid3kvrdi9okjcireiqm.apps.googleusercontent.com","project_id":"prime-haven-481407-a6","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost"]}}
+"
 
 flow = InstalledAppFlow.from_client_config(
     {"installed": {"client_id": client_id, "client_secret": client_secret, 
