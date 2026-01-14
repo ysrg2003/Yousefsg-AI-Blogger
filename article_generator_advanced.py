b9cbf9219ad6575629a6a95fb730d094bcf510f6
ysrg2003 <yousefghapn2003@gmail.com>
2026-01-14 14:06:27 +0200

    Refactor authentication to use refresh token method

--- article_generator_advanced.py
+++ article_generator_advanced.py
@@ -1,34 +1,21 @@
-import os
-from google_auth_oauthlib.flow import InstalledAppFlow
+import requests
 
-# البيانات الصحيحة مستخرجة من ملفك
 client_id = "291465728218-li940ggvqjc7jid3kvrdi9okjcireiqm.apps.googleusercontent.com"
 client_secret = "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"
+auth_code = "4/1ASc3gC2Dvyt7J-lbpPAEYuZbSWQrvqYmM2fhLoz6tWI4DASuaWX9or1pJ-o"
 
-def get_auth_url():
-    client_config = {
-        "installed": {
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token"
-        }
+def get_refresh_token():
+    params = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": auth_code,
+        "grant_type": "authorization_code",
+        "redirect_uri": "urn:ietf:wg:oauth:2.0:oob"
     }
-    
-    flow = InstalledAppFlow.from_client_config(
-        client_config,
-        scopes=['https://www.googleapis.com/auth/blogger']
-    )
-    
-    flow.redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
-    
-    # إضافة access_type='offline' ضرورية جداً للحصول على Refresh Token
-    auth_url, _ = flow.authorization_url(prompt='consent', access_type='offline')
-    
-    print("\n" + "="*50)
-    print("OPEN THIS URL IN YOUR BROWSER:")
-    print(auth_url)
-    print("="*50 + "\n")
+    r = requests.post("https://oauth2.googleapis.com/token", data=params)
+    print("\nYOUR REFRESH TOKEN IS:")
+    print(r.json().get('refresh_token'))
 
 if __name__ == "__main__":
-    get_auth_url()
+    get_refresh_token()
+
